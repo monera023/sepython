1b3af3c4e90c68844603b17dcd4b9b68081a4137
monera023 <sjituri@linkedin.com>
2024-06-26 23:11:20 +0530

    serving static files

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+console.log("Querying /api/search");
+fetch("/api/search", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'text/plain'
+  },
+  body: "bind texture, to buffer."
+}).then((response) => console.log(response));
